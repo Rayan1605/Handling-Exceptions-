3e6fe377d0bbbbbed5fad16116392e3e732fcbe5
rayan <rayanzyad99@gmail.com>
2022-12-10 18:31:55 +0000

    Code without tryandcatch

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Here you go");
+        Scanner myinput = new Scanner(System.in);
+        System.out.println("Please enter a number between 0-9");
+        int mynum = myinput.nextInt();
+        if (mynum <=9 && mynum>0){
+            System.out.println("You entered " + mynum);
+        }
+        else {
+            System.out.println("Not a valid number");
+        }
     }
 }
